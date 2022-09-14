3ed47a055629f806ef066d9918570a190deada9f
Dyanne Ihinosen <mathiasofeimun@gmail.com>
2022-09-14 14:57:36 +0100

    seventh

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -4,10 +4,10 @@ import "./MapStyles.css";
 import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css"; 
 import "./br.css"
 import ReactMapGL from "react-map-gl"; 
-import mapboxgl from "mapbox-gl"; // This is a dependency of react-map-gl even if you didn't explicitly install it
+// import mapboxgl from "mapbox-gl"; // This is a dependency of react-map-gl even if you didn't explicitly install it 
 
-// eslint-disable-next-line import/no-webpack-loader-syntax
-mapboxgl.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
+// // eslint-disable-next-line import/no-webpack-loader-syntax 6.1.9
+// mapboxgl.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
 
  
 
